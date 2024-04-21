f9f76c828fab8099e89b06028c77c69e21e0c336
J.D. Purcell <jdpurcell@gmail.com>
2024-04-21 13:56:01 -0400

    CI: Update Windows runner

--- dist/scripts/vcvars.ps1
+++ dist/scripts/vcvars.ps1
@@ -1,7 +1,7 @@
 # Script assumes $env:arch will start with win64 or win32
 # This should probably be an arg
 $arch = $env:arch.substring(3, 2)
-$path = Resolve-Path "${env:ProgramFiles(x86)}\Microsoft Visual Studio\*\*\VC\Auxiliary\Build" | select -ExpandProperty Path
+$path = Resolve-Path "${env:ProgramFiles}\Microsoft Visual Studio\*\*\VC\Auxiliary\Build" | select -ExpandProperty Path
 
 cmd.exe /c "call `"$path\vcvars$arch.bat`" && set > %temp%\vcvars.txt"
 
