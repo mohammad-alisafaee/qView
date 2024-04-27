e72b365d804d89d19f8ef299593245ab9f04cadd
J.D. Purcell <jdpurcell@gmail.com>
2024-04-27 15:47:02 -0400

    Return GitHub runners to macOS 14
    Worked around the issues by using specific Xcode versions.

--- dist/scripts/build.ps1
+++ dist/scripts/build.ps1
@@ -4,25 +4,28 @@ param (
     $Prefix = "/usr"
 )
 
-if ($IsWindows) {
-    dist/scripts/vcvars.ps1
-}
-
 $qtVersion = ((qmake --version -split '\n')[1] -split ' ')[3]
 Write-Host "Detected Qt Version $qtVersion"
+
+if ($IsWindows) {
+    dist/scripts/vcvars.ps1
+} elseif ($IsMacOS) {
+    if ($qtVersion -like '5.*') {
+        # Keep Qt 5.x build on Xcode 14 due to concern over QTBUG-117484
+        sudo xcode-select --switch /Applications/Xcode_14.3.1.app
+    } else {
+        # GitHub macOS 13/14 runners use Xcode 15.0.x by default which has a known linker issue causing crashes if the artifact is run on macOS <= 12
+        sudo xcode-select --switch /Applications/Xcode_15.3.app
+    }
+}
 
 $qmakeArgs = @(
     "PREFIX=""$Prefix""",
     "DEFINES+=""$env:nightlyDefines"""
 )
 
-if ($IsMacOS) {
-    if ($qtVersion -like '5.*') {
-        # QTBUG-117225
-        $qmakeArgs += @("-early", "QMAKE_DEFAULT_LIBDIRS=""$(xcrun -show-sdk-path)/usr/lib""")
-    } else {
-        $qmakeArgs += "QMAKE_APPLE_DEVICE_ARCHS=""x86_64 arm64"""
-    }
+if ($IsMacOS -and $qtVersion -notlike '5.*') {
+    $qmakeArgs += "QMAKE_APPLE_DEVICE_ARCHS=""x86_64 arm64"""
 }
 
 Write-Host "Running 'qmake' w/ args: $qmakeArgs"
