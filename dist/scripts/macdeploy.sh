458709920303a8ca1b896c7165de4a028935c9ec
J.D. Purcell <jdpurcell@gmail.com>
2024-04-29 00:39:26 -0400

    macOS deployment improvements
    * Fix warnings/errors with macdeployqt
    * Reduce size of DMG
    * Put version in DMG volume name

--- dist/scripts/macdeploy.sh
+++ dist/scripts/macdeploy.sh
@@ -1,22 +1,24 @@
 #!/usr/bin/bash
 
-if [ $1 != "" ]; then
-        VERSION=$0
+if [[ -n "$1" ]]; then
+    VERSION=$0
 else
-        VERSION=$(LC_ALL=C sed -n -e '/^VERSION/p' qView.pro)
-        VERSION=${VERSION: -3}
+    VERSION=$(LC_ALL=C sed -n -e '/^VERSION/p' qView.pro)
+    VERSION=${VERSION: -3}
 fi
 
 cd bin
 
 macdeployqt qView.app
-if [ $1 != "" ]; then
-    macdeployqt *.app -codesign=- -dmg
-    mv qView.dmg qView-JDP-$1-macOS$2.dmg
+codesign --sign - --deep qView.app
+if [[ -n "$1" ]]; then
+    BUILD_NAME=qView-JDP-$1
+    hdiutil create -srcfolder "qView.app" -volname "$BUILD_NAME" -format UDSB "qView.sparsebundle"
+    hdiutil convert "qView.sparsebundle" -format ULFO -o "$BUILD_NAME-macOS$2.dmg"
+    rm -r qView.sparsebundle
 else
     brew install create-dmg
-    codesign --sign - --deep qView.app
-    create-dmg --volname "qView $VERSION" --window-size 660 400 --icon-size 160 --icon "qView.app" 180 170 --hide-extension qView.app --app-drop-link 480 170 "qView-JDP-$VERSION.dmg" "qView.app"
+    create-dmg --volname "qView-JDP $VERSION" --filesystem APFS --format ULFO --window-size 660 400 --icon-size 160 --icon "qView.app" 180 170 --hide-extension qView.app --app-drop-link 480 170 "qView-JDP-$VERSION.dmg" "qView.app"
 fi
 
 rm -r *.app
