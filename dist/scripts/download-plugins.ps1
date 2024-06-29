ba0710d201a9134aa73114417733ac9bec796dbe
J.D. Purcell <jdpurcell@gmail.com>
2024-06-29 15:52:23 -0400

    Win32 AVIF decoder update

--- dist/scripts/download-plugins.ps1
+++ dist/scripts/download-plugins.ps1
@@ -68,13 +68,8 @@ if ($pluginNames -contains 'KImageFormats') {
     if ($IsWindows) {
         mv KImageFormats/KImageFormats/output/kimg_*.dll "$out_imf/"
         CopyFrameworkDlls "KF$($kfMajorVer)Archive.dll" @("zlib1.dll")
-        if ($env:buildArch -eq 'X86') {
-            CopyFrameworkDlls "avif.dll" @("aom.dll")
-            CopyFrameworkDlls "heif.dll" @("aom.dll", "libde265.dll")
-        } else {
-            CopyFrameworkDlls "avif.dll" @("dav1d.dll")
-            CopyFrameworkDlls "heif.dll" @("libde265.dll")
-        }
+        CopyFrameworkDlls "avif.dll" @("dav1d.dll")
+        CopyFrameworkDlls "heif.dll" @("libde265.dll")
         CopyFrameworkDlls "raw.dll" @("lcms2.dll", "zlib1.dll")
         CopyFrameworkDlls "jxl.dll" @("brotlicommon.dll", "brotlidec.dll", "brotlienc.dll", "hwy.dll", "jxl_cms.dll", "jxl_threads.dll", "lcms2.dll")
         CopyFrameworkDlls "OpenEXR-3_2.dll" @("deflate.dll", "Iex-3_2.dll", "IlmThread-3_2.dll", "Imath-3_1.dll", "OpenEXRCore-3_2.dll")
