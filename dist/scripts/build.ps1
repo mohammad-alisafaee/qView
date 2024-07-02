7e892010ff7bf16b603b7c39ca130f47d6462488
J.D. Purcell <jdpurcell@gmail.com>
2024-07-01 22:26:22 -0400

    Windows: Workaround for crashing with older Visual C++ runtimes

--- dist/scripts/build.ps1
+++ dist/scripts/build.ps1
@@ -19,8 +19,17 @@ if ($IsWindows) {
     }
 }
 
-$argDeviceArchs = $IsMacOS -and $env:buildArch -eq 'Universal' ? "QMAKE_APPLE_DEVICE_ARCHS=x86_64 arm64" : $null
-qmake PREFIX="$Prefix" DEFINES+="$env:nightlyDefines" $argDeviceArchs
+if ($IsMacOS) {
+    $argDeviceArchs =
+        $env:buildArch -eq 'X64' ? 'QMAKE_APPLE_DEVICE_ARCHS=x86_64' :
+        $env:buildArch -eq 'Arm64' ? 'QMAKE_APPLE_DEVICE_ARCHS=arm64' :
+        $env:buildArch -eq 'Universal' ? 'QMAKE_APPLE_DEVICE_ARCHS=x86_64 arm64' :
+        $null
+} elseif ($IsWindows) {
+    # Workaround for https://developercommunity.visualstudio.com/t/10664660
+    $argVcrMutexWorkaround = 'DEFINES+=_DISABLE_CONSTEXPR_MUTEX_CONSTRUCTOR'
+}
+qmake PREFIX="$Prefix" DEFINES+="$env:nightlyDefines" $argVcrMutexWorkaround $argDeviceArchs
 
 if ($IsWindows) {
     nmake
