mohammad-alisafaee/qView

commit 88c1904c3f9e17f44a1ebf8fd7508321f4b674a2

Pan image when resizing window to maintain focal point

src/qvgraphicsview.cpp
@@ -72,6 +72,9 @@ void QVGraphicsView::resizeEvent(QResizeEvent *event)
             return;
 
     QGraphicsView::resizeEvent(event);
+
+    const QPoint sizeDelta = QRect(QPoint(), event->size()).bottomRight() - QRect(QPoint(), event->oldSize()).bottomRight();
+    scrollHelper->move(QPointF(sizeDelta) / -2.0);
     fitOrConstrainImage();
 }
 

commit f676d4951e02252bb5a9f9f20c72de848abb9b95

Fix regression with macOS session restore
Image was at incorrect position due to 88c1904 which caused the viewport to scroll even before an image was loaded.

src/qvgraphicsview.cpp
@@ -73,9 +73,12 @@ void QVGraphicsView::resizeEvent(QResizeEvent *event)
 
     QGraphicsView::resizeEvent(event);
 
-    const QPoint sizeDelta = QRect(QPoint(), event->size()).bottomRight() - QRect(QPoint(), event->oldSize()).bottomRight();
-    scrollHelper->move(QPointF(sizeDelta) / -2.0);
-    fitOrConstrainImage();
+    if (getCurrentFileDetails().isPixmapLoaded)
+    {
+        const QPoint sizeDelta = QRect(QPoint(), event->size()).bottomRight() - QRect(QPoint(), event->oldSize()).bottomRight();
+        scrollHelper->move(QPointF(sizeDelta) / -2.0);
+        fitOrConstrainImage();
+    }
 }
 
 void QVGraphicsView::paintEvent(QPaintEvent *event)
@@ -509,7 +512,7 @@ void QVGraphicsView::reloadFile()
 void QVGraphicsView::beforeLoad()
 {
     // If a prior pixmap is still loaded, capture its content rect
-    if (!loadedPixmapItem->pixmap().isNull())
+    if (getCurrentFileDetails().isPixmapLoaded)
         lastImageContentRect = getContentRect();
 }
 