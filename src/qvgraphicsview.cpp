mohammad-alisafaee/qView

commit 99afdaaca885cd0e97301a9fa30f20d428d6a28b

Add "Set Zoom Level" feature

src/qvgraphicsview.cpp
@@ -602,30 +602,32 @@ void QVGraphicsView::postLoad()
 
 void QVGraphicsView::zoomIn()
 {
-    zoomRelative(zoomMultiplier);
+    zoomRelative(zoomMultiplier, Qv::CalculateViewportCenterPos);
+    fitOrConstrainImage();
 }
 
 void QVGraphicsView::zoomOut()
 {
-    zoomRelative(qPow(zoomMultiplier, -1));
+    zoomRelative(qPow(zoomMultiplier, -1), Qv::CalculateViewportCenterPos);
+    fitOrConstrainImage();
 }
 
 void QVGraphicsView::zoomRelative(const qreal relativeLevel, const std::optional<QPoint> &mousePos)
 {
-    const qreal absoluteLevel = zoomLevel * relativeLevel;
-
-    if (absoluteLevel >= 500 || absoluteLevel <= 0.01)
-        return;
-
-    zoomAbsolute(absoluteLevel, mousePos);
+    const qreal absoluteLevel = std::clamp(zoomLevel * relativeLevel, 0.01, 100.0);
+    const std::optional<QPoint> pos = !mousePos.has_value() ? std::nullopt : zoomToCursor && isCursorVisible ? mousePos : Qv::CalculateViewportCenterPos;
+    zoomAbsolute(absoluteLevel, pos);
 }
 
-void QVGraphicsView::zoomAbsolute(const qreal absoluteLevel, const std::optional<QPoint> &mousePos, const bool isApplyingCalculation)
+void QVGraphicsView::zoomAbsolute(const qreal absoluteLevel, const std::optional<QPoint> &targetPos, const bool isApplyingCalculation)
 {
     if (!isApplyingCalculation || !Qv::calculatedZoomModeIsSticky(calculatedZoomMode.value()))
         setCalculatedZoomMode({});
 
-    const std::optional<QPoint> pos = !mousePos.has_value() ? std::nullopt : zoomToCursor && isCursorVisible ? mousePos : getUsableViewportRect().center();
+    if (absoluteLevel == zoomLevel)
+        return;
+
+    const std::optional<QPoint> pos = targetPos == Qv::CalculateViewportCenterPos ? getUsableViewportRect().center() : targetPos;
     if (pos != lastZoomEventPos)
     {
         lastZoomEventPos = pos;

src/actionmanager.cpp
@@ -279,6 +279,7 @@ QMenu *ActionManager::buildViewMenu(QWidget *parent)
 
     addCloneOfAction(viewMenu, "zoomin");
     addCloneOfAction(viewMenu, "zoomout");
+    addCloneOfAction(viewMenu, "zoomcustom");
     addCloneOfAction(viewMenu, "originalsize");
     addCloneOfAction(viewMenu, "zoomtofit");
     addCloneOfAction(viewMenu, "fillwindow");
@@ -720,6 +721,8 @@ void ActionManager::actionTriggered(QAction *triggeredAction, MainWindow *releva
         relevantWindow->zoomIn();
     } else if (key == "zoomout") {
         relevantWindow->zoomOut();
+    } else if (key == "zoomcustom") {
+        relevantWindow->zoomCustom();
     } else if (key == "originalsize") {
         relevantWindow->originalSize();
     } else if (key == "zoomtofit") {
@@ -862,6 +865,10 @@ void ActionManager::initializeActionLibrary()
     auto *zoomOutAction = new QAction(qvApp->iconFromFont(Qv::MaterialIcon::ZoomOut), tr("Zoom &Out"));
     zoomOutAction->setData({"disable"});
     actionLibrary.insert("zoomout", zoomOutAction);
+
+    auto *zoomCustomAction = new QAction(qvApp->iconFromFont(Qv::MaterialIcon::Search), tr("Set Zoom &Level..."));
+    zoomCustomAction->setData({"disable"});
+    actionLibrary.insert("zoomcustom", zoomCustomAction);
 
     auto *originalSizeAction = new QAction(qvApp->iconFromFont(Qv::MaterialIcon::LooksOne), tr("Ori&ginal Size"));
     originalSizeAction->setData({"disable"});

src/mainwindow.cpp
@@ -1192,6 +1192,16 @@ void MainWindow::zoomOut()
     graphicsView->zoomOut();
 }
 
+void MainWindow::zoomCustom()
+{
+    bool ok;
+    const double oldValue = graphicsView->getZoomLevel() * 100.0;
+    const double newValue = QInputDialog::getDouble(this, tr("Set Zoom Level"), tr("Zoom Level (%):"), oldValue, 1.0, 10000.0, 1, &ok);
+    if (!ok) return;
+    graphicsView->zoomAbsolute(newValue / 100.0, Qv::CalculateViewportCenterPos);
+    graphicsView->fitOrConstrainImage();
+}
+
 void MainWindow::originalSize()
 {
     graphicsView->setCalculatedZoomMode(Qv::CalculatedZoomMode::OriginalSize);

src/mainwindow.h
@@ -95,6 +95,8 @@ public:
 
     void zoomOut();
 
+    void zoomCustom();
+
     void originalSize();
 
     void setZoomToFit(const bool value);

src/qvgraphicsview.h
@@ -41,7 +41,7 @@ public:
 
     void zoomRelative(const qreal relativeLevel, const std::optional<QPoint> &mousePos = {});
 
-    void zoomAbsolute(const qreal absoluteLevel, const std::optional<QPoint> &mousePos = {}, const bool isApplyingCalculation = false);
+    void zoomAbsolute(const qreal absoluteLevel, const std::optional<QPoint> &targetPos = {}, const bool isApplyingCalculation = false);
 
     const std::optional<Qv::CalculatedZoomMode> &getCalculatedZoomMode() const;
     void setCalculatedZoomMode(const std::optional<Qv::CalculatedZoomMode> &value, const bool isNavigating = false);

src/qvnamespace.h
@@ -13,6 +13,8 @@ namespace Qv
 
     inline constexpr int SessionStateVersion = 1;
 
+    inline constexpr QPoint CalculateViewportCenterPos(std::numeric_limits<int>::max(), std::numeric_limits<int>::max());
+
     // Settings value enums
 
     enum class AfterDelete
@@ -179,6 +181,7 @@ namespace Qv
         RotateLeft = 0xE419,
         RotateRight = 0xE41A,
         Save = 0xE161,
+        Search = 0xE8B6,
         SettingsOverscan = 0xE8C4,
         Settings = 0xE8B8,
         Shuffle = 0xE043,

src/shortcutmanager.cpp
@@ -85,6 +85,7 @@ void ShortcutManager::initializeShortcutsList()
         shortcutsList.last().defaultShortcuts << QKeySequence(Qt::CTRL | Qt::Key_Equal).toString();
 
     shortcutsList.append({tr("Zoom Out"), "zoomout", keyBindingsToStringList(QKeySequence::ZoomOut), {}});
+    shortcutsList.append({tr("Set Zoom Level"), "zoomcustom", {}, {}});
     shortcutsList.append({tr("Original Size"), "originalsize", QStringList(QKeySequence(Qt::CTRL | Qt::Key_0).toString()), {}});
     shortcutsList.append({tr("Zoom to Fit"), "zoomtofit", QStringList(QKeySequence(Qt::CTRL | Qt::Key_9).toString()), {}});
     shortcutsList.append({tr("Fill Window"), "fillwindow", QStringList(QKeySequence(Qt::CTRL | Qt::Key_8).toString()), {}});

commit 733069f7f1a75abb4d74d0ddebbe404bb24132a8

Fix image not recentering after hiding titlebar
Regression in 99afdaaca885cd0e97301a9fa30f20d428d6a28b

src/qvgraphicsview.cpp
@@ -624,9 +624,7 @@ void QVGraphicsView::zoomAbsolute(const qreal absoluteLevel, const std::optional
     if (!isApplyingCalculation || !Qv::calculatedZoomModeIsSticky(calculatedZoomMode.value()))
         setCalculatedZoomMode({});
 
-    if (absoluteLevel == zoomLevel)
-        return;
-
+    const bool isChanging = absoluteLevel != zoomLevel;
     const std::optional<QPoint> pos = targetPos == Qv::CalculateViewportCenterPos ? getUsableViewportRect().center() : targetPos;
     if (pos != lastZoomEventPos)
     {
@@ -662,9 +660,12 @@ void QVGraphicsView::zoomAbsolute(const qreal absoluteLevel, const std::optional
         centerImage();
     }
 
-    handleSmoothScalingChange();
+    if (isChanging)
+    {
+        handleSmoothScalingChange();
 
-    emit zoomLevelChanged();
+        emit zoomLevelChanged();
+    }
 }
 
 const std::optional<Qv::CalculatedZoomMode> &QVGraphicsView::getCalculatedZoomMode() const